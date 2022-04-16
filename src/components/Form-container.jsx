7fd6283f8635fdf01d11acf11830c8be1ccddf4f
guibrbs <200037994@aluno.unb.br>
2022-04-16 20:46:04 -0300

    :rocket:Adding icons

--- src/components/Form-container.jsx
+++ src/components/Form-container.jsx
@@ -7,7 +7,29 @@ export default function FormContainer() {
                 <form action="" className="sign-in-form">
                     <h2 className="tittle">Sign in</h2>
                     <div className="input-field">
-                        
+                        <i className="fas fa-user"></i>
+                        <input type="text" placeholder="Username"/>
+                    </div>
+                    <div className="input-field">
+                        <i className="fas fa-lock"></i>
+                        <input type="password" placeholder="Password"/>
+                    </div>
+                    <input type="submit" value="Login" className="btn solid" />
+
+                    <p className="social-text">Or sign in with social plataforms</p>
+                    <div className="social-media">
+                        <a href="#" className="social-icon">
+                            <i className="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#" className="social-icon">
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                        <a href="#" className="social-icon">
+                            <i className="fab fa-google"></i>
+                        </a>
+                        <a href="#" className="social-icon">
+                            <i className="fab fa-linkedin-in"></i>
+                        </a>
                     </div>
                 </form>
             </div>
