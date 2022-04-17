52b7fc1d26b1916b0e9352a5fc2de1628d1566cb
guibrbs <200037994@aluno.unb.br>
2022-04-17 02:11:37 -0300

    :rocket:Setting up sign up container

--- src/components/Form-container.jsx
+++ src/components/Form-container.jsx
@@ -33,6 +33,39 @@ export default function FormContainer() {
                         </a>
                     </div>
                 </form>
+
+                <form action="" className="sign-up-form">
+                    <h2 className="title">Sign up</h2>
+                    <div className="input-field">
+                        <i className="fas fa-user"></i>
+                        <input type="text" placeholder="Username"/>
+                    </div>
+                    <div className="input-field">
+                        <i className="fas fa-envelope"></i>
+                        <input type="text" placeholder="Email"/>
+                    </div>
+                    <div className="input-field">
+                        <i className="fas fa-lock"></i>
+                        <input type="password" placeholder="Password"/>
+                    </div>
+                    <input type="submit" value="Sign up" className="btn solid" />
+
+                    <p className="social-text">Or sign up with social plataforms</p>
+                    <div className="social-media">
+                        <a href="#" className="social-icon">
+                            <i className="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#" className="social-icon">
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                        <a href="#" className="social-icon">
+                            <i className="fab fa-google"></i>
+                        </a>
+                        <a href="#" className="social-icon">
+                            <i className="fab fa-linkedin-in"></i>
+                        </a>
+                    </div>
+                </form>
             </div>
         </div>
     )
